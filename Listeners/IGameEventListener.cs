4e78f23a4cfc6061cd59168e0f2a9c72c44ff59b
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 19:43:29 -0300

    Interface Game Event Listener

---
+++ Listeners/IGameEventListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arekushi.GameEvent
+{
+    public interface IGameEventListener<T>
+	{
+        void OnEventRaised(T item);
+    }
+}
