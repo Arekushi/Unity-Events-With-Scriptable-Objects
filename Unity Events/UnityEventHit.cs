6a9665fa7816ad61b512679f40f0e4d65acf9a31
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 19:43:45 -0300

    Unity Event Hit

---
+++ Unity Events/UnityEventHit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Arekushi.GameEvent
+{
+    [Serializable]
+    public class HitUnityEvent : UnityEvent<HitData> { }
+}
