9f69c1517105b4e50e2ffce512fe2a096a270e8a
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 19:42:14 -0300

    Hit Event

---
+++ Events/HitEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Arekushi.GameEvent
+{
+
+    public class HitData
+	{
+
+        public GameObject aggressor;
+        public GameObject victim;
+        public int damage;
+
+        public HitData(GameObject aggressor, GameObject victim, int damage)
+		{
+            this.aggressor = aggressor;
+            this.victim = victim;
+            this.damage = damage;
+        }
+    }
+
+    [CreateAssetMenu(menuName = "Characters/Events/Hit Event")]
+    public class HitEvent : BaseEvent<HitData> { }
+}
