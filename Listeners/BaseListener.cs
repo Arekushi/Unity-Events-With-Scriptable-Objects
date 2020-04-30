5418aabc18ebdd1274138a3be210ffb8c74e0c80
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 19:42:42 -0300

    Base Listener Monobehaviour

---
+++ Listeners/BaseListener.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Arekushi.GameEvent
+{
+
+    public abstract class BaseListener<T, E, UER> : MonoBehaviour, IGameEventListener<T>
+        where E : BaseEvent<T> where UER : UnityEvent<T>
+		{
+
+        [SerializeField]
+        private E gameEvent;
+
+        [SerializeField]
+        private UER unityEventResponse;
+
+        private void OnEnable()
+		{
+            if (gameEvent != null)
+                gameEvent.RegisterListener(gameObject, this);
+        }
+
+        private void OnDisable()
+		{
+            if (gameEvent != null)
+                gameEvent.UnRegisterListener(gameObject, this);
+        }
+
+        public void OnEventRaised(T item)
+		{
+            if(unityEventResponse != null)
+                unityEventResponse.Invoke(item);
+        }
+
+    }
+}
