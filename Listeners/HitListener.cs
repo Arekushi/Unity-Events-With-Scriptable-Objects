4ea1c4d32c1c410cfd9e4b59465df9ee95904cd3
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 19:43:01 -0300

    Hit Listener

---
+++ Listeners/HitListener.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arekushi.GameEvent
+{
+    public class HitListener : BaseListener<HitData, HitEvent, HitUnityEvent> { }
+}
